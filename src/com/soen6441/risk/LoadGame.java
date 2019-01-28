f91f90420b88efb52e0b533630be53d0628e7fc1
NagaSatishReddyD <dnagasatishreddy@gmail.com>
2019-01-28 17:09:20 -0500

    players info screen added commit

--- src/com/soen6441/risk/LoadGame.java
+++ src/com/soen6441/risk/LoadGame.java
@@ -3,12 +3,21 @@ package com.soen6441.risk;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.HeadlessException;
+import java.awt.Image;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.awt.event.ActionEvent;
 
 /**
@@ -18,7 +27,7 @@ import java.awt.event.ActionEvent;
  */
 public class LoadGame extends JFrame {
 
-	private JPanel contentPane;
+	private JPanel startContentPanel;
 	LoadGame frame;
 
 	/**
@@ -45,15 +54,58 @@ public class LoadGame extends JFrame {
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		setContentPane(contentPane);
+		startContentPanel = new JPanel();
+		setContentPane(startContentPanel);
 		
 		JButton startGameButton = new JButton("Start Game");
+		
 		startGameButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("Game loading starts....");
+				startButtonActionListener();
+			}
+
+		});
+		
+		
+		startContentPanel.add(startGameButton, BorderLayout.CENTER);
+	}
+	
+	
+	private void startButtonActionListener() {
+		setBounds(100, 100, 450, 300);
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 593, 381);
+		JPanel playerInfoPanel = new JPanel();
+		setContentPane(playerInfoPanel);
+		
+		
+		try {
+			BufferedImage bufferedImage = ImageIO.read(new File(System.getProperty("user.dir")+RiskGameConstants.MAP_IMAGE));
+			Image scaledImage = bufferedImage.getScaledInstance(150, 150, Image.SCALE_SMOOTH);
+			ImageIcon mapImageIcon = new ImageIcon(scaledImage);
+			JLabel imageLabel = new JLabel();
+			imageLabel.setIcon(mapImageIcon);
+			playerInfoPanel.add(imageLabel);
+		} catch (IOException e1) {
+			System.out.println("Image not found...");
+		}
+		
+		String [] playersNumbers = {"2","3","4","5","6"};
+		JComboBox playerCountCombo = new JComboBox(playersNumbers);
+		playerCountCombo.setSelectedIndex(0);
+		
+		JButton loadGameButton = new JButton("Load Game");
+		loadGameButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		contentPane.add(startGameButton, BorderLayout.CENTER);
+		
+		JLabel playersCountLabel = new JLabel("Players Count");
+		playerInfoPanel.add(playersCountLabel);
+		
+		playerInfoPanel.add(playerCountCombo);
+		playerInfoPanel.add(loadGameButton);	
 	}
 }
---
+++ src/com/soen6441/risk/RiskGameConstants.java
@@ -0,0 +1,11 @@
+package com.soen6441.risk;
+
+/**
+ * RiskGameConstants class contains the constants in the game
+ * @author Naga Satish Reddy
+ *
+ */
+public class RiskGameConstants {
+
+	public static String MAP_IMAGE = "/Map.jpg";
+}
