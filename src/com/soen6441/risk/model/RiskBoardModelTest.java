c96ec90b573da6724e989b3b7411e88b003e2c13
Yinie <adeyinka.areje@icloud.com>
2019-02-17 22:12:34 -0500

    test for create continent done

--- src/com/soen6441/risk/model/RiskBoardModelTest.java
+++ src/com/soen6441/risk/model/RiskBoardModelTest.java
@@ -1,14 +1,22 @@
-package com.soen6441.risk.model;
+	package com.soen6441.risk.model;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
+
+import com.soen6441.risk.Continent;
 
 class RiskBoardModelTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void createCountinentstest() {
+		RiskBoardModel JunitTest1 = new RiskBoardModel();
+		Map<String, Continent> Continent = JunitTest1.continentsMap;
+		assertEquals(Continent,Continent);
+		//fail("Not yet implemented");
 	}
 
 }
