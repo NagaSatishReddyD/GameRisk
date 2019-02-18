6c1960cc2a836ba64ac293ee8801ff66f513f7e2
Yinie <adeyinka.areje@icloud.com>
2019-02-17 21:22:03 -0500

    Junit test one for riskboard controller created

---
+++ src/com/soen6441/risk/controller/RiskBoardControllerTest.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.soen6441.risk.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Yinie
+ *
+ */
+class RiskBoardControllerTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
