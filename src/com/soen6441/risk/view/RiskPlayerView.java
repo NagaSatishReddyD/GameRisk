dcdfea0871c638955d6a88d09fdb847f7b8b8e66
An Nguyen <nnthienan1994@gmail.com>
2019-02-24 14:53:26 -0500

    Add a combo box to choose a map to play

--- src/com/soen6441/risk/view/RiskPlayerView.java
+++ src/com/soen6441/risk/view/RiskPlayerView.java
@@ -1,5 +1,7 @@
 package com.soen6441.risk.view;
 
+import java.io.File;
+
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -11,6 +13,7 @@ public class RiskPlayerView extends JFrame {
 	private JFrame playerStartframe;
 	private JComboBox playerCountCombo;
 	private JButton loadGameButton;
+	private JComboBox mapComboBox;
 
 
 	/**
@@ -30,13 +33,32 @@ public class RiskPlayerView extends JFrame {
 		String [] playersNumbers = {"2","3","4","5","6"};
 		playerCountCombo = new JComboBox(playersNumbers);
 		playerCountCombo.setSelectedIndex(0);
-		playerCountCombo.setBounds(286, 21, 97, 21);
+		playerCountCombo.setBounds(286, 21, 121, 21);
 		playerStartframe.getContentPane().add(playerCountCombo);
 		
 		
 		loadGameButton = new JButton("Load Game");
-		loadGameButton.setBounds(170, 62, 213, 21);
+		loadGameButton.setBounds(166, 118, 213, 21);
 		playerStartframe.getContentPane().add(loadGameButton);
+		
+		JLabel mapLabel = new JLabel("Choose map");
+		mapLabel.setBounds(166, 74, 78, 16);
+		playerStartframe.getContentPane().add(mapLabel);
+		
+		String[] filesName = null;
+		filesName = getAllMapFiles(filesName);
+		mapComboBox = new JComboBox(filesName);
+		
+		mapComboBox.setBounds(286, 70, 121, 27);
+		playerStartframe.getContentPane().add(mapComboBox);
+	}
+
+	public JComboBox getMapComboBox() {
+		return mapComboBox;
+	}
+
+	public void setMapComboBox(JComboBox mapComboBox) {
+		this.mapComboBox = mapComboBox;
 	}
 
 	public JFrame getPlayerStartframe() {
@@ -66,5 +88,15 @@ public class RiskPlayerView extends JFrame {
 
 	public void setLoadGameButton(JButton loadGameButton) {
 		this.loadGameButton = loadGameButton;
+	}
+	
+	public String[] getAllMapFiles(String[] filesName) {
+		File directory = new File(System.getProperty("user.dir")+"/resources/");
+		File[] files = directory.listFiles((dir,name) -> name.endsWith(".map"));
+		filesName = new String[files.length];
+		for(int i = 0; i < files.length; i++) {
+			filesName[i] = files[i].getName();
+		}
+		return filesName;
 	}
 }
