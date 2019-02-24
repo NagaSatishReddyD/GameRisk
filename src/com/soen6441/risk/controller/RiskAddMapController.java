497dbf24c2baee83ccd876db66ec4c4a2d8b8ba0
An Nguyen <nnthienan1994@gmail.com>
2019-02-24 15:59:29 -0500

    Add controller file for add map

---
+++ src/com/soen6441/risk/controller/RiskAddMapController.java
@@ -0,0 +1,46 @@
+package com.soen6441.risk.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import com.soen6441.risk.model.RiskAddMapModel;
+import com.soen6441.risk.view.RiskAddMapView;
+import com.soen6441.risk.view.RiskGameView;
+
+public class RiskAddMapController {
+
+	RiskAddMapModel model;
+	RiskAddMapView view;
+	
+	public RiskAddMapController(RiskAddMapModel riskAddMapModel, RiskAddMapView riskAddMapView) {
+		this.model = riskAddMapModel;
+		this.view = riskAddMapView;
+	}
+	
+	public void addMap(RiskGameView gameView) {
+		view.getFrame().setVisible(true);
+		this.view.getMapUploadBtn().addActionListener(e -> {
+			try {
+				JFileChooser chooser = new JFileChooser();
+				chooser.setMultiSelectionEnabled(true);
+				FileNameExtensionFilter filter = new FileNameExtensionFilter("Map files", "map", "bmp");
+				chooser.setFileFilter(filter);
+				chooser.showOpenDialog(null);
+				File[] files = chooser.getSelectedFiles();
+				model.mapValidation(view, files);
+			} catch (NumberFormatException exception) {
+				System.out.println(exception.getMessage());
+			}
+		});
+		
+		this.view.getBackBtn().addActionListener(e -> {
+			view.getFrame().setVisible(false);
+			gameView.getGameStartframe().setVisible(true);
+		});
+	}
+
+}
