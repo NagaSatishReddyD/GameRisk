f283808ccc0cfd4722c38588d3d31ae358f92679
An Nguyen <nnthienan1994@gmail.com>
2019-02-24 16:13:49 -0500

    Add model file for add map

---
+++ src/com/soen6441/risk/model/RiskAddMapModel.java
@@ -0,0 +1,25 @@
+package com.soen6441.risk.model;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import com.soen6441.risk.view.RiskAddMapView;
+
+public class RiskAddMapModel {
+	
+	/**
+	 * This method is used to validate the correctness of the uploaded map.
+	 * @param view
+	 * @param files
+	 */
+	public void mapValidation(RiskAddMapView view, File[] files) {
+		//in progress
+		view.getFrame().setVisible(false);
+		for(int i = 0; i < files.length; i++) {
+			System.out.println(files[i].getName());
+		}
+	}
+}
