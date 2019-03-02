f7de7f215a6f87d0c1f3d490d826b85e686ce93a
An Nguyen <nnthienan1994@gmail.com>
2019-03-01 20:38:15 -0500

    Added mapValidation method

--- src/com/soen6441/risk/model/RiskAddMapModel.java
+++ src/com/soen6441/risk/model/RiskAddMapModel.java
@@ -5,8 +5,13 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+import com.soen6441.risk.controller.RiskPlayerController;
 import com.soen6441.risk.view.RiskAddMapView;
+import com.soen6441.risk.view.RiskPlayerView;
 
 
 /**
@@ -23,9 +28,38 @@ public class RiskAddMapModel {
 	 */
 	public void mapValidation(RiskAddMapView view, File[] files) {
 		//in progress
+		RiskBoardModel model = new RiskBoardModel();
+		if(files.length != 2 || (files[0].getName().endsWith("bmp") && files[1].getName().endsWith("bmp")) || (files[0].getName().endsWith("map") && files[1].getName().endsWith("map"))) {
+			return;
+		}
 		view.getFrame().setVisible(false);
-		for(int i = 0; i < files.length; i++) {
-			System.out.println(files[i].getName());
+		
+		try {
+			
+			Path sourceImage;
+			Path destinationImage;
+			if(files[0].getName().endsWith("map")) {
+				model.loadRequiredData(files[0].getAbsolutePath());
+				Path sourceMap = Paths.get(files[0].getAbsolutePath());
+				Path destinationMap = Paths.get(System.getProperty("user.dir")+"/resources/"+files[0].getName());
+				Files.copy(sourceMap, destinationMap);
+				sourceImage = Paths.get(files[1].getAbsolutePath());
+				destinationImage = Paths.get(System.getProperty("user.dir")+"/resources/"+files[1].getName());
+			}else {
+				model.loadRequiredData(files[1].getAbsolutePath());
+				Path sourceMap = Paths.get(files[1].getAbsolutePath());
+				Path destinationMap = Paths.get(System.getProperty("user.dir")+"/resources/"+files[1].getName());
+				Files.copy(sourceMap, destinationMap);
+				sourceImage = Paths.get(files[0].getAbsolutePath());
+				destinationImage = Paths.get(System.getProperty("user.dir")+"/resources/"+files[0].getName());
+			}
+			Files.copy(sourceImage, destinationImage);
+			RiskPlayerModel riskPlayerModel = new RiskPlayerModel();
+			RiskPlayerView riskPlayerView = new RiskPlayerView();
+			RiskPlayerController riskPlayerController = new RiskPlayerController(riskPlayerModel, riskPlayerView);
+			riskPlayerController.initalizeGame();
+		}catch(IOException e) {
+			
 		}
 	}
 }
