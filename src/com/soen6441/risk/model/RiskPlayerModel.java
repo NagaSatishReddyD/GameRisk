55901c5615dd86b2849444ac61f2d6de0fc249aa
NagaSatishReddyD <dnagasatishreddy@gmail.com>
2019-03-02 17:50:49 -0500

    added java doc for RiskPlayerModel file commit

--- src/com/soen6441/risk/model/RiskPlayerModel.java
+++ src/com/soen6441/risk/model/RiskPlayerModel.java
@@ -7,8 +7,21 @@ import com.soen6441.risk.controller.RiskBoardController;
 import com.soen6441.risk.view.RiskBoardView;
 import com.soen6441.risk.view.RiskPlayerView;
 
+/**
+ * RiskPlayerModel class handles the actions of the RiskPlayerModel View.
+ * @author Naga Satish Reddy
+ * @since 1.0
+ *
+ */
 public class RiskPlayerModel {
 
+	/**
+	 * startBoardFrame method is used to go the RiskBoardView from the RiskPlayerView
+	 * @param view, instance of the RiskPlayerView
+	 * @throws IOException
+	 * @throws NumberFormatException
+	 * @throws NoSuchAlgorithmException
+	 */
 	public void startBoardFrame(RiskPlayerView view) throws IOException, NumberFormatException, NoSuchAlgorithmException {
 		view.getPlayerStartframe().setVisible(false);
 		RiskBoardModel riskBoardModel = new RiskBoardModel();
